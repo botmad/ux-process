40f0036ee7455c11447976d83dddac11d2dcaf23
botmad <botmad@users.noreply.github.com>
2021-08-11 13:10:10 +0100

    webpack config edit to put images inside a folder

--- webpack.config.js
+++ webpack.config.js
@@ -61,7 +61,7 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+            // Since sass-loader has SCSS as its default parse mode, we map
             // the "scss" and "sass" values for the lang attribute to the right configs here.
             // other preprocessors should work out of the box, no loader config like this necessary.
             'scss': [
@@ -87,7 +87,7 @@ module.exports = {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file-loader',
         options: {
-          name: '[name].[ext]?[hash]'
+          name: 'images/[name].[ext]?[hash]'
         }
       }
     ]
